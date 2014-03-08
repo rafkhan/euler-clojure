178113b7273a487b076df8d582afe46a06f551ca
Rafael Khan <raf.khan.f@gmail.com>
2014-03-08 17:14:36 -0500

    Formatting?

--- 1.clj
+++ 1.clj
@@ -1,9 +1,8 @@
 (println
-  (reduce +
+  (reduce + 
     (filter 
-      (fn [x] 
-        (or 
-            (= 0 (mod x 3)) 
-          (= 0 (mod x 5))))
+      (fn [x] (or 
+        (= 0 (mod x 3)) 
+        (= 0 (mod x 5))))
       (range 1000))))
 
